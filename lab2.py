ba71581705027af6e85150bdfa67c274d1e6ab5d
sava666 <saviak.nazar@gmail.com>
2017-09-21 14:28:38 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+print('Input youre\n')
+
+name = input('Name:\n')
+lastname = input('Lastname:\n')
+fathername = input('Fathername:\n')
+
+print('Hellow,' +' '+ name+' ' + lastname +' '+ fathername)
