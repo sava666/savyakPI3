0d90c31acfd55f85563582e19f9543b0ee02d21f
sava666 <saviak.nazar@gmail.com>
2017-09-21 17:44:32 +0300

    Add files via upload

---
+++ lab33.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+import sys
+
+weight =float(input('Enter youre weight in kilograms\n'))
+height=float(input('Enter youre height in meters\n'))
+BMI=weight/(height**2)
+BMI=str(round(BMI,1))
+print('Youre BMI (Body Mass Index) = ' + BMI)
+
+
+
+
+
+
+
+
+
+
